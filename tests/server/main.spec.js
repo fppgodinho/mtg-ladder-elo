3f674a073c9327dcb2342696eca0aecde14f74fe
Fernando Godinho <fppgodinho@gmail.com>
2017-03-29 21:37:18 +0000

    Update main.spec.js

--- tests/server/main.spec.js
+++ tests/server/main.spec.js
@@ -1,6 +1,6 @@
 var should = require('chai').should();
 var Main = require('./../../server/main.js');
-
+godinho1
 describe('The Main Class', function() {
   it('Should exist', function() {
     Main.should.exist;
